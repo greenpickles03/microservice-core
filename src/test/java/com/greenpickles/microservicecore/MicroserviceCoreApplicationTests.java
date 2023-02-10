edcea9bb56dc4cc44d6bd7e130fd4def96a4abca
greenpickles03 <71419350+greenpickles03@users.noreply.github.com>
2023-02-10 13:59:55 +0800

    Initial Commit

---
+++ src/test/java/com/greenpickles/microservicecore/MicroserviceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenpickles.microservicecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
